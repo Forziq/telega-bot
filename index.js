0f46c45273bca67290a4d8ce44cc00b04b86c8ff
PD1m0n123 <di50012dp@gmail.com>
2023-02-12 19:36:18 +0200

    всё+

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const TelegramBot = require("node-telegram-bot-api");
-const token = "";
+const token = "6090942940:AAFAZgEoCa3Y27f1dCQuxk-zX24pZ2V55Y4";
 const bot = new TelegramBot(token, {polling: true});
