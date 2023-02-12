04ad7867158d7fbef15785d3eb4cb7dc99c32763
Forzammil <forzammil@gmail.com>
2023-02-12 17:48:02 +0000

    openai

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require("node-telegram-bot-api");
-const openai = require('openai');
+const openai = require("openai");
 const token = "6090942940:AAFAZgEoCa3Y27f1dCQuxk-zX24pZ2V55Y4";
 const bot = new TelegramBot(token, {polling: true});
 
