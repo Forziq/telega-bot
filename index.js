ff50751c8e85edafe199dd461dbb5db7bd8f1579
Forzammil <forzammil@gmail.com>
2023-02-12 18:00:15 +0000

    index

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const TelegramBot = require("node-telegram-bot-api");
-const openai = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 const token = "6090942940:AAFAZgEoCa3Y27f1dCQuxk-zX24pZ2V55Y4";
 const bot = new TelegramBot(token, {polling: true});
+const OPENAI_API_KEY = "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"
 
-openai.prompt(
-  {
-    model: "text-davinci-002",
-    prompt: "What is the meaning of life?"
-  },
-  function(error, result) {
-    console.log(result.text);
-  }
-);
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Say this is a test",
+  temperature: 0,
+  max_tokens: 7,
+});
